3f8371fa24bfe5feb0081e79c37d29deeaf6557c
HilbertSpecs <evanjames1990@gmail.com>
2021-12-14 02:01:55 +0000

    analyze_review_sentiments() Error Trace 404

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -23,7 +23,7 @@ def get_request(url, **kwargs):
 
             response = requests.get(url,params=params,headers={'Content-Type': 'application/json'}, auth=HTTPBasicAuth('apikey',kwargs["api_key"]))
 
-            print("Response: ",response)
+            print("Response_apikey_Provided: ",response)
 
         else:
             print("No api_key passed to get request.")
